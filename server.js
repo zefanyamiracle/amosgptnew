96d624806ef739fdb3d761d424ed7ddfd18aa540
zefanyamiracle <zefanyamiracle777@gmail.com>
2024-07-12 13:21:24 +0700

    Change Code, Bug Fixing add Favicon File

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv').config();
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5500;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
 
